a488442fa26b0a4ffca39e36babfec2d1a97ceaa
Sharath Ciddu <sharath.ciddu@rubrik.com>
2023-03-13 16:58:03 -0700

    Add setup_mac.sh Add setup_ubuntu.sh

---
+++ executable_setup_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+install_zshtools() {
+	sudo apt install -y autojump
+	sudo apt install -y tmux
+	sudo apt install -y pyenv
+}
+
+install_pyenv() {
+	sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+		libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+		libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev curl
+	curl https://pyenv.run | bash
+}
+
+if [ `uname` != "Linux" ]; then
+    echo "Run on Linux (not on Mac OS X)"; exit 1
+fi
+
+sudo apt update
+install_zshtools
+install_pyenv
