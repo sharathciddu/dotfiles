ae17a8ee21fc5195a0a1d89708fa4593c0368094
Sharath Ciddu <sharathciddu@gmail.com>
2023-04-19 10:34:30 -0700

    Update .oh-my-zsh/custom/aliases.zsh

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -3,7 +3,7 @@ alias cz="chezmoi"
 
 # Git
 alias gpor="git pull origin --rebase"
-alias git-local-branch-cleanup="git branch | grep -v\"master\" | xargs git branch -D"
+alias git-local-branch-cleanup="git branch | grep -v \"master\" | xargs git branch -D"
 alias git-reset-all="git submodule foreach --recursive git reset --hard"
 alias git-vendor-reset="git submodule init && git submodule sync && git submodule update --init -recursive"
 
